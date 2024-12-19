1ea16416104cb00cf038f673628b4f7f1fb10435
Phyo Than Htike <6531503173@lamduan.mfu.ac.th>
2024-12-20 01:20:00 +0700

    Auth

--- backend/index.js
+++ backend/index.js
@@ -2,20 +2,21 @@ import express from "express";
 import dotenv from 'dotenv';
 import authRoute from "./routes/auth.route.js"; // Adjust path if needed
 import { connectDB } from "./lib/db.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config()
 
 const app = express();
 
-// Middleware to use the auth route
-app.use("/api/auth", authRoute);
-
 // Start the server
 const PORT = process.env.PORT;
 
-app.use(express.json())
+app.use(express.json());
+app.use(cookieParser());
+// Middleware to use the auth route
+app.use("/api/auth", authRoute);
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-  connectDB()
+  console.log(`Server running on port ${PORT}`);
+  connectDB();
 });
